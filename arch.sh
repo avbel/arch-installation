2ad1f0cc7b564fa5f2c8aa3e6ede5d8fd492d610
Andrey Belchikov <belchikovs@gmail.com>
2022-01-18 11:53:04 +0300

    my config

--- arch.sh
+++ arch.sh
@@ -5,14 +5,16 @@
 ################################################################################
 install_disk=/dev/nvme0n1
 boot_partition=/dev/nvme0n1p1
-root_partition=/dev/nvme0n1p2
-encrypt_partition=/dev/mapper/archlinux
+swap_partition=/dev/nvme0n1p2
+root_partition=/dev/nvme0n1p3
+home_partition=/dev/nvme0n1p4
+#encrypt_partition=/dev/mapper/archlinux
 
 ################################################################################
 #### Dialog function                                                        ####
 ################################################################################
 installer_dialog() {
-    DIALOG_RESULT=$(whiptail --clear --backtitle "Meyvin's Arch Installer" "$@" 3>&1 1>&2 2>&3)
+    DIALOG_RESULT=$(whiptail --clear --backtitle "Arch Installer" "$@" 3>&1 1>&2 2>&3)
     DIALOG_CODE=$?
 }
 
@@ -27,7 +29,7 @@ fi
 #### Welcome                                                                ####
 ################################################################################
 clear
-installer_dialog --title "Welcome" --msgbox "\nWelcome to Meyvin's Arch Linux Installer." 10 60
+installer_dialog --title "Welcome" --msgbox "\nWelcome to Arch Linux Installer." 10 60
 
 ################################################################################
 #### User account Prompts                                                   ####
@@ -38,7 +40,7 @@ installer_dialog --title "Root password" --passwordbox "\nEnter a strong passwor
 root_password="$DIALOG_RESULT"
 installer_cancel
 
-installer_dialog --title "Root account" --msgbox "\nCreate a user account.\n" 10 60
+installer_dialog --title "User account" --msgbox "\nCreate a user account.\n" 10 60
 
 installer_dialog --title "username" --inputbox "\nPlease enter a username for your home account.\n" 10 60
 user_name="$DIALOG_RESULT"
@@ -52,9 +54,9 @@ installer_dialog --title "User accounts" --msgbox "\nDone setting up accounts.\n
 ################################################################################
 #### Password prompts                                                       ####
 ################################################################################
-installer_dialog --title "Disk encryption" --passwordbox "\nEnter a strong passphrase for the disk encryption." 10 60
-encryption_passphrase="$DIALOG_RESULT"
-installer_cancel
+#installer_dialog --title "Disk encryption" --passwordbox "\nEnter a strong passphrase for the disk encryption." 10 60
+#encryption_passphrase="$DIALOG_RESULT"
+#installer_cancel
 
 ################################################################################
 #### Hostname host                                                              ####
@@ -101,42 +103,45 @@ sgdisk ${install_disk} -o
 
 echo "Creating EFI Partition"
 sgdisk ${install_disk} -n 1::+512MiB -t 1:ef00
-
+echo "Creating Swap partition"
+sgdisk ${install_disk} -n 2::32G -t 1:8200
 echo "Creating Root partition"
-sgdisk ${install_disk} -n 2 -t 1:8e00
+sgdisk ${install_disk} -n 3::100G -t 1:8e00
+echo "Creating Home partition"
+sgdisk ${install_disk} -n 4 -t 1:8302
 
 echo "Format EFI partition"
 mkfs.vfat ${boot_partition}
 
-if [[ ! -z $encryption_passphrase ]]; then
-    echo "Setting up encryption"
-    printf "%s" "$encryption_passphrase" | cryptsetup luksFormat ${root_partition}
-    printf "%s" "$encryption_passphrase" | cryptsetup luksOpen ${root_partition} archlinux
-    mkinitcpio_hooks="encrypt lvm2"
-    lvm_volume="${encrypt_partition}"
-else
-    exit 0
-fi
+#if [[ ! -z $encryption_passphrase ]]; then
+#    echo "Setting up encryption"
+#    printf "%s" "$encryption_passphrase" | cryptsetup luksFormat ${root_partition}
+#    printf "%s" "$encryption_passphrase" | cryptsetup luksOpen ${root_partition} archlinux
+#    mkinitcpio_hooks="encrypt lvm2"
+#    lvm_volume="${encrypt_partition}"
+#else
+#    exit 0
+#fi
 
-echo "Setting up LVM"
-vgcreate vg1 $lvm_volume
-lvcreate -L 60G vg1 -n root
-lvcreate -l 100%FREE vg1 -n home
+#echo "Setting up LVM"
+#vgcreate vg1 $lvm_volume
+#lvcreate -L 60G vg1 -n root
+#lvcreate -l 100%FREE vg1 -n home
 
 echo "Creating filesystems and enabling swap"
 mkfs.vfat /dev/nvme0n1p1
-mkfs.ext4 /dev/vg1/root
-mkfs.ext4 /dev/vg1/home
-mkswap /dev/vg1/swap
+mkfs.btrfs -L root /dev/nvme0n1p3
+mkfs.btrfs -L home /dev/nvme0n1p4
+mkswap /dev/nvme0n1p2
 
 ################################################################################
 #### Install Arch                                                           ####
 ################################################################################
-mount /dev/vg1/root /mnt
+mount /dev/nvme0n1p3 /mnt
 mkdir /mnt/home
-mount /dev/vg1/home /mnt/home
+mount /dev/nvme0n1p4 /mnt/home
 mkdir /mnt/boot
-mount ${boot_partition} /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
 
 yes '' | pacstrap -i /mnt base linux linux-firmware git vim $cpu_ucode lvm2
 
@@ -155,7 +160,7 @@ export LANG=en_US.UTF-8
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 echo "Setting time zone"
-ln -s /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+ln -s /usr/share/zoneinfo/Europe/Moscow /etc/localtime
 
 echo "Setting hostname"
 echo $hostname > /etc/hostname
@@ -165,7 +170,7 @@ echo "Disabling annoying pc speaker"
 echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
 
 echo "Generating initramfs"
-sed -i "s/^HOOKS.*/HOOKS=\(base udev autodetect modconf block keyboard ${mkinitcpio_hooks} filesystems keyboard fsck\)/" /etc/mkinitcpio.conf
+sed -i "s/^HOOKS.*/HOOKS=\(base udev autodetect modconf block keyboard ${mkinitcpio_hooks} filesystems keyboard fsck btrfs\)/" /etc/mkinitcpio.conf
 
 mkinitcpio -g /boot/initramfs-linux.img -k $kernel_version
 
@@ -176,6 +181,9 @@ echo "Enable Colors, Parallel Downloads and Multilib in /etc/pacman.conf"
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 sed -i '/Color/s/^#//g' /etc/pacman.conf
 sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
+echo [home-thaodan] >> /etc/pacman.conf
+echo Server = https://thaodan.de/public/archlinux/home-thaodan/x86_64/ >> /etc/pacman.conf
+
 
 echo "Updating pacman"
 pacman -Syuu
@@ -205,11 +213,13 @@ alsa-utils \
 avahi \
 base-devel \
 bash-completion \
+btrfs-progs \
+bees \
 bluez \
 bluez-utils \
 cups \
 dialog \
-dnsmasq \
+#dnsmasq \
 dnsutils \
 dosfstools \
 ebtables \
@@ -219,16 +229,16 @@ flatpak \
 $gpu_drivers \
 gvfs \
 gvfs-smb \
-hplip \
+#hplip \
 inetutils \
 ipset \
 iptables-nft \
-linux-headers \
+#linux-headers \
 network-manager-applet \
 networkmanager \
 nfs-utils \
 nss-mdns \
-ntfs-3g \
+#ntfs-3g \
 openbsd-netcat \
 openssh \
 pipewire \
@@ -237,6 +247,8 @@ pipewire-jack \
 pipewire-pulse \
 reflector \
 rsync \
+rclone \
+restic \
 smbclient \
 sof-firmware \
 terminus-font \
@@ -244,14 +256,18 @@ tlp \
 wpa_supplicant \
 xdg-user-dirs \
 xdg-utils \
-
+linux-pf \
+ksmbd-tools \
+wireguard-tools \
+snapper \
+snap-pac \
+snap-sync \
 arch-chroot /mnt /bin/bash <<EOF
 
 timedatectl set-ntp true
 hwclock --systohc
 
 systemctl enable NetworkManager 
-systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 systemctl enable avahi-daemon
@@ -269,7 +285,7 @@ EOF
 ################################################################################
 #### EFIStub                                                                ####
 ################################################################################
-efibootmgr --disk ${root_partition} --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "cryptdevice=${root_partition}:archlinux:allow-discards root=/dev/vg1/root rw initrd=\\${cpu_ucode}.img initrd=\initramfs-linux.img" --verbose
+efibootmgr --disk ${root_partition} --create --label "Arch Linux" --loader /vmlinuz-linux --unicode "root=${root_partition} rw initrd=\\${cpu_ucode}.img initrd=\initramfs-linux.img" --verbose
 
 ################################################################################
 #### The end                                                                ####
--- sway.sh
+++ sway.sh
@@ -3,7 +3,7 @@
 ################################################################################
 #### Mirrors and Pacman.conf configuration                                  ####
 ################################################################################
-MIRRORCOUNTRY="Netherlands"
+MIRRORCOUNTRY="Germany"
 
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
@@ -11,10 +11,10 @@ sudo hwclock --systohc
 echo "Retrieve and filter the latest Pacman mirror list for ${MIRRORCOUNTRY}"
 sudo reflector -c $MIRRORCOUNTRY -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 
-echo "Setting up Firewall"
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+#echo "Setting up Firewall"
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
 
 ################################################################################
 #### Pacman packages                                                        ####
@@ -30,13 +30,14 @@ sudo pacman -Syu
 echo "Installing Sway Desktop Environment"
 sudo pacman -S \
 alacritty \
-blueman \
+#blueman \
 docker \
 docker-compose \
+libsecret \
 file-roller \
 firefox-developer-edition \
-gedit \
-gedit-plugins \
+#gedit \
+#gedit-plugins \
 gnome-calculator \
 gnome-keyring \
 jq \
@@ -44,6 +45,8 @@ playerctl \
 polkit-gnome \
 qt5-wayland \
 qt6-wayland \
+qt5ct \
+qt6ct \
 smbclient \
 slurp \
 sway \
@@ -52,16 +55,37 @@ thunar \
 thunar-archive-plugin \
 thunar-media-tags-plugin \
 thunar-volman \
-thunderbird \
+#thunderbird \
 tumbler \
-unrar \
+#unrar \
+imv \
 wf-recorder \
+wdisplays \
 wget \
 xorg-xwayland \
 zathura \
 zathura-cb \
+zathura-djvu \
 zathura-pdf-mupdf \
-zsh
+zsh \
+bitwarden \
+wayvnc \
+mailnag  \
+#nodejs \
+#npm \
+kdeconnect \
+shotwell \
+digikam \
+imv \
+pinta \
+youtube-dl \
+tig \
+#neovim \
+#neovim-qt \
+#powerline \
+diffuse \
+bat \
+glances 
 
 ################################################################################
 #### Paru aur package manager installation                                  ####
@@ -81,34 +105,60 @@ adobe-base-14-fonts \
 arc-gtk-theme-git \
 celluloid \
 clipman \
-gammastep \
-gitflow-avh \
+#gammastep \
+git \
+#gitflow-avh \
 grim \
-intellij-idea-ultimate-edition \
-intellij-idea-ultimate-edition-jre \
-kanshi \
+#intellij-idea-ultimate-edition \
+#intellij-idea-ultimate-edition-jre \
+#kanshi \
 mako-git \
 nerd-fonts-complete \
 nnn-nerd \
 otf-monaco-powerline-font-git \
 otf-font-awesome \
-postman-bin \
-rambox-bin \
+#postman-bin \
+#rambox-bin \
 siji \
-spotify \
+#spotify \
 swappy \
 sway-audio-idle-inhibit-git \
 swaylock-effects \
-sxiv \
 tela-icon-theme \
 ttf-material-design-icons-desktop-git \
 visual-studio-code-bin \
 waybar \
 wofi \
-wps-office \
+nvm \
+#wps-office \
 xcursor-simp1e \
 yadm-git \
 zramd \
+way-displays \
+#slack-desktop \
+ttf-ms-win11 \
+google-chrome \
+chromedriver \
+#v4l2loopback-dkms-git \
+kyocera_universal \
+epson-inkjet-printer-stylus-photo-t50-series \
+wlrobs \
+obs-studio \
+bitwarden-rofi \
+insomnia-bin \
+slack-wayland \
+chromium-wayland-vaapi \
+alvr \
+peazip-qt-bin \
+masterpdfeditor-free \
+qeh-git \
+lightzone \
+mellowplayer \
+kdiff3-qt \
+rar \
+extramaus \
+qt5-styleplugins \
+qt6gtk2 \
 
 ################################################################################
 #### Enabling Docker                                                        ####
@@ -133,12 +183,12 @@ gsettings set org.gnome.desktop.interface icon-theme 'Tela-dark'
 #### Installing Dotfiles                                                    ####
 ################################################################################
 echo "Installing Dotfiles"
-cd ~;yadm clone https://github.com/meyvin/dotfiles.git
+cd ~;yadm clone https://github.com/avbel/dotfiles-1.git
 
 ################################################################################
 #### Enable systemd services                                                ####
 ################################################################################
-sudo systemctl enable --user kanshi
+#sudo systemctl enable --user kanshi
 sudo systemctl enable zramd
 
 ################################################################################
